f1572b44c4a64f8152233495ff96e11c12fb5175
Stacy <stacello19@gmail.com>
2020-07-14 14:27:16 -0400

    path join

--- stacify-back/routes/index.js
+++ stacify-back/routes/index.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const path = require('path');
 
+router.use(express.static(path.join(__dirname, 'build')));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Stacify Backend' });
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 
